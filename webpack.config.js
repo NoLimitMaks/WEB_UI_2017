75d16e1ec20402cdeede3d1f80c64e5fd7aded28
Maks Hlushek <NoLimitMaks@users.noreply.github.com>
2017-11-15 18:24:39 +0200

    update json to latest  packages & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var config = {
         include: SRC_DIR,
         loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015', 'stage-2']
+          presets: ['react', 'env', 'stage-2']
         }
       },
 
